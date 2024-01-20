4d02fe0507853680eb166cc93e18127fc7b4eb46
logicalmechanism <logical.mechanism@protonmail.com>
2024-01-20 12:24:34 -0800

    adding script documentation

--- complete_build.sh
+++ complete_build.sh
@@ -10,7 +10,7 @@ rm contracts/* || true
 rm hashes/* || true
 rm -fr build/ || true
 
-# build out the entire script
+# start building out the entire script
 echo -e "\033[1;34m\nBuilding Contracts\n\033[0m"
 
 # standard build
@@ -23,7 +23,6 @@ aiken build
 ###############################################################################
 ###############################################################################
 
-# build out the entire script
 echo -e "\033[1;34m\nBuilding FT Contract \033[0m"
 
 # the locking token information
@@ -38,11 +37,16 @@ locking_tkn_cbor=$(python3 -c "import cbor2;hex_string='${locking_tkn}';data = b
 random_string=$(LC_ALL=C tr -dc a-f0-9 </dev/urandom | head -c 32)
 random_cbor=$(python3 -c "import cbor2;hex_string='${random_string}';data = bytes.fromhex(hex_string);encoded = cbor2.dumps(data);print(encoded.hex())")
 
+echo Random String 1: ${random_string}
+
 echo -e "\033[1;33m Convert Perma Lock FT Contract \033[0m"
 
+# apply the parameters to the contract
 aiken blueprint apply -o plutus.json -v perma_lock_ft.params "${locking_pid_cbor}"
 aiken blueprint apply -o plutus.json -v perma_lock_ft.params "${locking_tkn_cbor}"
 aiken blueprint apply -o plutus.json -v perma_lock_ft.params "${random_cbor}"
+
+# store the plutus file in the contracts folder
 aiken blueprint convert -v perma_lock_ft.params > contracts/perma_lock_ft_contract.plutus
 
 ###############################################################################
@@ -55,15 +59,21 @@ echo -e "\033[1;34m\nBuilding NFT Contract \033[0m"
 random_string=$(LC_ALL=C tr -dc a-f0-9 </dev/urandom | head -c 32)
 random_cbor=$(python3 -c "import cbor2;hex_string='${random_string}';data = bytes.fromhex(hex_string);encoded = cbor2.dumps(data);print(encoded.hex())")
 
+echo Random String 2: ${random_string}
+
 echo -e "\033[1;33m Convert Perma Lock NFT Contract \033[0m"
+
+# apply the parameters to the contract
 aiken blueprint apply -o plutus.json -v perma_lock_nft.params "${random_cbor}"
+
+# store the plutus file in the contracts folder
 aiken blueprint convert -v perma_lock_nft.params > contracts/perma_lock_nft_contract.plutus
 
 ###############################################################################
 ###############################################################################
 ###############################################################################
 
-# store the script hash
+# store the script hashes in the hashes folder
 echo -e "\033[1;34m\nBuilding Contract Hash Data \033[0m"
 
 cardano-cli transaction policyid --script-file contracts/perma_lock_ft_contract.plutus > hashes/perma_lock_ft.hash
